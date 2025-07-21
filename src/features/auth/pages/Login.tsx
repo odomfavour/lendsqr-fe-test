0e64d9181738c2f4776f641b63d7bc581eb4f364
ogochuks-aac <ogochukwu@adebayoadeleke.com>
2025-07-21 14:53:12 +0100

    ch: added route to login temporarily

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,12 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import styles from '../auth.module.scss';
 
 const Login = () => {
-  console.log('styles', styles);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // prevent default form reload
+
+    navigate('/users');
+  };
   return (
     <div className={styles.loginBox}>
       <h1>Welcome!</h1>
       <p>Enter details to login.</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className={styles.inputWrapper}>
           <input type="email" placeholder="Email" />
         </div>
